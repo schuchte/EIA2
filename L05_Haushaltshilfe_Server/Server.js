58b0fe7315b3d640dd11d38c9c0586bfa2311d66
schuchte <tanja.schuchter@hs-furtwangen.de>
2020-06-04 14:54:40 +0200

    lul

--- L05_Haushaltshilfe_Server/Server.js
+++ L05_Haushaltshilfe_Server/Server.js
@@ -2,17 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Http = require("http");
 const Url = require("url");
-const Mongo = require("mongodb");
+const Mongo = require("mongodb"); //Mongo beliebig gewählter Name
 var L07_Househelp;
 (function (L07_Househelp) {
     let orders;
     let port = process.env.PORT;
     if (port == undefined)
         port = 5001;
-    let databaseUrl = "mongodb+srv://Tata:<Amsterdam99>@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority";
+    let databaseUrl = "mongodb+srv://Tata:Amsterdam99@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority"; //Adresse von Datenbankserver
     //mongodb+srv://Tata:<Amsterdam99>@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority
     startServer(port);
-    connectToDatabase(databaseUrl);
+    connectToDatabase(databaseUrl); //gibt Adresse weiter an Funktion connectToDatabase
     function startServer(_port) {
         let server = Http.createServer();
         console.log("Server starting on port:" + _port);
@@ -21,23 +21,23 @@ var L07_Househelp;
     }
     async function connectToDatabase(_url) {
         let options = { useNewUrlParser: true, useUnifiedTopology: true };
-        let mongoClient = new Mongo.MongoClient(_url, options);
+        let mongoClient = new Mongo.MongoClient(_url, options); //wir kriegen hier ne Art MongoShell die mit unserer Datenbank kommuniziert
         await mongoClient.connect();
-        orders = mongoClient.db("Househelp").collection("Orders");
-        console.log("Database connection ", orders != undefined);
+        orders = mongoClient.db("Househelp").collection("Orders"); //Eingreifen und schon mal eine Datenbank (Househelp) und eine Collection darin (Orders) erstellen
+        console.log("Database connection ", orders != undefined); //wenn das geklappt hat Konsolenausgabe mit den Daten
     }
     function handleRequest(_request, _response) {
         console.log("What's up?");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
-            let url = Url.parse(_request.url, true);
+            let url = Url.parse(_request.url, true); //wandelt query String in Objekt um
             for (let key in url.query) {
                 _response.write(key + ":" + url.query[key] + "<br/>");
             }
-            let jsonString = JSON.stringify(url.query);
+            let jsonString = JSON.stringify(url.query); //
             _response.write(jsonString);
-            storeOrder(url.query);
+            storeOrder(url.query); //nehmen den query von der url --> einen Teil vom url (?Drink=Mojito....)
         }
         _response.end();
     }
--- L05_Haushaltshilfe_Server/Server.ts
+++ L05_Haushaltshilfe_Server/Server.ts
@@ -1,6 +1,6 @@
 import * as Http from "http";
 import * as Url from "url";
-import * as Mongo from "mongodb";
+import * as Mongo from "mongodb"; //Mongo beliebig gewählter Name
 
 export namespace L07_Househelp {
     interface Order {
@@ -13,12 +13,12 @@ export namespace L07_Househelp {
     if (port == undefined)
         port = 5001;
 
-    let databaseUrl: string = "mongodb+srv://Tata:Amsterdam99@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority";
+    let databaseUrl: string = "mongodb+srv://Tata:Amsterdam99@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority"; //Adresse von Datenbankserver
 
-    //mongodb+srv://Tata:Amsterdam99@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority
+    //mongodb+srv://Tata:<Amsterdam99>@eia2-9msb9.mongodb.net/test?retryWrites=true&w=majority
 
     startServer(port);
-    connectToDatabase(databaseUrl);
+    connectToDatabase(databaseUrl); //gibt Adresse weiter an Funktion connectToDatabase
 
     function startServer(_port: number | string): void {
         let server: Http.Server = Http.createServer();
@@ -28,12 +28,12 @@ export namespace L07_Househelp {
         server.addListener("request", handleRequest);
     }
 
-    async function connectToDatabase(_url: string): Promise<void> {
+    async function connectToDatabase(_url: string): Promise<void> { //die url ist der Link an den Datenbankserver
         let options: Mongo.MongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
-        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options); //wir kriegen hier ne Art MongoShell die mit unserer Datenbank kommuniziert
         await mongoClient.connect();
-        orders = mongoClient.db("Househelp").collection("Orders");
-        console.log("Database connection ", orders != undefined);
+        orders = mongoClient.db("Househelp").collection("Orders"); //Eingreifen und schon mal eine Datenbank (Househelp) und eine Collection darin (Orders) erstellen
+        console.log("Database connection ", orders != undefined); //wenn das geklappt hat Konsolenausgabe mit den Daten
     }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
@@ -43,15 +43,15 @@ export namespace L07_Househelp {
         _response.setHeader("Access-Control-Allow-Origin", "*");
 
         if (_request.url) {
-            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true); //wandelt query String in Objekt um
             for (let key in url.query) {
                 _response.write(key + ":" + url.query[key] + "<br/>");
             }
 
-            let jsonString: string = JSON.stringify(url.query);
+            let jsonString: string = JSON.stringify(url.query); //
             _response.write(jsonString);
 
-            storeOrder(url.query);
+            storeOrder(url.query);//nehmen den query von der url --> einen Teil vom url (?Drink=Mojito....)
         }
 
         _response.end();
